2ac5a45070f536edcccdbe2b3a845c24504a5219
Patrick Boyd <pat.m.boyd@gmail.com>
2014-04-08 12:07:03 -0700

    Draw graphviz of nearby words

--- src/Main.java
+++ src/Main.java
@@ -8,10 +8,12 @@ class Main {
 		options.addOption( new Option( "h", "help", false, "print help" ) );
 		options.addOption( new Option( "s", "start_word", false, "print random start word" ) );
 		options.addOption( new Option( "a", "adjacent_word", true, "print words within 1 letter of entered word") );
+		options.addOption( new Option( "pg", "print_graph_dot", true, "print .dot file of word graph") );
 
 		return options;
 	}
 	public static void main(String[] args) throws ParseException {
+		String dict_location = "../dictionaries/TWL06.txt";
 		CommandLineParser parser = new GnuParser();
 		Options options = Main.getOptions();
 
@@ -20,11 +22,14 @@ class Main {
 		if (cmd.hasOption("help") ) {
 			new HelpFormatter().printHelp("Main", options);
 		} else if (cmd.hasOption("start_word")) {
-			ScrabbleSlam game = new ScrabbleSlam();
+			ScrabbleSlam game = new ScrabbleSlam(dict_location);
 			System.out.println( game.pickRandomStartWord() );
 		} else if (cmd.hasOption("adjacent_word")) {
-			ScrabbleSlam game = new ScrabbleSlam();
+			ScrabbleSlam game = new ScrabbleSlam(dict_location);
 			System.out.println( game.adjacentWords(cmd.getOptionValue("adjacent_word") ) );
+		} else if (cmd.hasOption("print_graph_dot")) {
+			ScrabbleSlam game = new ScrabbleSlam(dict_location);
+			System.out.println( game.dotGraph(cmd.getOptionValue("print_graph_dot")) );
 		} 
 	}
 }
--- src/scrabbleslam/ScrabbleSlam.java
+++ src/scrabbleslam/ScrabbleSlam.java
@@ -1,5 +1,6 @@
 package scrabbleslam;
 
+import java.lang.StringBuilder;
 import java.util.Random;
 import java.util.HashSet;
 import scrabbleslam.Dictionary;
@@ -7,13 +8,13 @@ import scrabbleslam.Dictionary;
 public class ScrabbleSlam {
 		private Dictionary dict;
 
-		public ScrabbleSlam() {
+		public ScrabbleSlam(String dict_location) {
 				dict = new Dictionary(new Dictionary.NewWordFilter() {
 						public boolean can_add(String s) {
 								return s.length() == 4;
 						}
 				});
-				dict.load("../dictionaries/TWL06.txt");
+				dict.load(dict_location);
 		}
 		public HashSet<String> adjacentWords(String word) {
 				return dict.findAdjacentWords(word);
@@ -37,5 +38,21 @@ public class ScrabbleSlam {
 						}
 				}
 				return candidate;
+		}
+
+		public String dotGraph(String search_word) {
+			StringBuilder output = new StringBuilder();
+			output.append("graph Words {\n");
+			output.append("rankdir=LR;\n");
+
+			for (String adj_word: dict.findAdjacentWords(search_word)) {
+				output.append("\"" + search_word +  "\" -- \"" + adj_word + "\";\n");
+				for (String sub_adj_word: dict.findAdjacentWords(adj_word)) {
+					output.append("\"" + adj_word + "\" -- \"" + sub_adj_word+ "\";\n");
+				}
+			}
+
+			output.append("}");
+			return output.toString();
 		}
 }
